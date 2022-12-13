5ceb90568718c49c5ea4356d53412cfef621d0fd
Luka Andelic <andelic-pm@mothership.de>
2022-12-13 09:18:06 +0100

    added school project

---
+++ rest-workbook-students/src/main/java/dev/bsinfo/ressource/HelloWorlRessource.java
@@ -0,0 +1,17 @@
+package dev.bsinfo.ressource;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+
+
+@Path("world")
+public class HelloWorlRessource {
+
+	@Path("hello")
+	@GET
+	//@Produces(MediaType.TEXT_PLAIN)
+	public String getHelloWorld() {
+		return "Hello World";
+	}
+	
+}
